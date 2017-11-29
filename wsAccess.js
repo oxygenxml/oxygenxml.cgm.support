1bc6d5268e1a74e475c55241877e6581aab0ddd2
mihai_coanda <coanda.mihai@gmail.com>
2017-11-29 14:27:49 +0200

    WA-1484 Left the oXygen built-in CGM Image Handler to make it compatible with the WebAuthor.

--- wsAccess.js
+++ wsAccess.js
@@ -1,5 +1,5 @@
 function applicationStarted(pluginWorkspaceAccess) {
- imageHandler = {
+ var imageHandler = {
       editImage: function(editContext){return null;},
       editImage: function(url){return false;},
       canHandleFileType: function(type){return type.equals("cgm")},
@@ -12,21 +12,28 @@ function applicationStarted(pluginWorkspaceAccess) {
          return getCGMImage(pluginWorkspaceAccess, cp, rc);
       }
  }
- pluginWorkspaceAccess.getImageUtilities().addImageHandler(new JavaAdapter(Packages.ro.sync.exml.workspace.api.images.handlers.EditImageHandler, imageHandler));
+
+
+  /**
+   * This line overrides the oXygen default CGM image handler.
+   *
+   * Uncomment to activate this feature.
+   */
+  //pluginWorkspaceAccess.getImageUtilities().addImageHandler(new JavaAdapter(Packages.ro.sync.exml.workspace.api.images.handlers.EditImageHandler, imageHandler));
 }
 
 function getCGMImage(pluginWorkspaceAccess, cp, rc){
-     cgmPanel = new Packages.net.sf.jcgm.core.CGMPanel();
+     var cgmPanel = new Packages.net.sf.jcgm.core.CGMPanel();
      cgmPanel.setSize(800, 600);
      cgmPanel.open(cp.getUrl());
     // Create a new bufferedImage with imagePanel size
-    image = new Packages.java.awt.image.BufferedImage(800, 600, Packages.java.awt.image.BufferedImage.TYPE_INT_RGB);
+    var image = new Packages.java.awt.image.BufferedImage(800, 600, Packages.java.awt.image.BufferedImage.TYPE_INT_RGB);
     // Create a graphics context from the new image
-    graphics = image.createGraphics();
+    var graphics = image.createGraphics();
     graphics.setColor(Packages.java.awt.Color.WHITE);
     graphics.fillRect(0, 0, 800, 600);
-    
-    paintComponentMethod = getPaintMethod(cgmPanel.getClass());
+
+    var paintComponentMethod = getPaintMethod(cgmPanel.getClass());
     paintComponentMethod.setAccessible(true);
     paintComponentMethod.invoke(cgmPanel, graphics);
      return image;
@@ -34,15 +41,15 @@ function getCGMImage(pluginWorkspaceAccess, cp, rc){
 
   /**
    * Looks inside the provided class to find the "paint" method.
-   * 
-   * @param clazz The class with public "paint" method. 
+   *
+   * @param clazz The class with public "paint" method.
    * @return The "paint" method.
    */
   function getPaintMethod(clazz) {
-    paint = null;
+    var paint = null;
     while (paint == null && clazz != null) {
       try {
-        declMethods = clazz.getDeclaredMethods();
+        var declMethods = clazz.getDeclaredMethods();
         for(i=0;i<declMethods.length;i++){
             if(declMethods[i].getName().equals("paint")){
                 paint = declMethods[i];
@@ -51,10 +58,10 @@ function getCGMImage(pluginWorkspaceAccess, cp, rc){
       } catch (e) {
         // No such method.
       }
-      
+
       clazz = clazz.getSuperclass();
     }
-    
+
     return paint;
   }
 
